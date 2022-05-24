feed277af648695aac6f7c49a879b27d8946e317
Manatee <github@osuosl.org>
2022-05-24 17:47:19 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the proj-seagl cookbook.
 
+1.0.0 (2022-05-24)
+------------------
+- Initial Default Recipe
+
 ## 0.1.0
 
 - Initial release
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs/Configures proj-seagl'
 issues_url        'https://github.com/osuosl-cookbooks/proj-seagl/issues'
 source_url        'https://github.com/osuosl-cookbooks/proj-seagl'
 chef_version      '>= 16.0'
-version           '0.1.0'
+version           '1.0.0'
 
 depends           'base'
 depends           'users'
